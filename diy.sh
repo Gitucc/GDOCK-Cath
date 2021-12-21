44cd3c44cad00992ec20366339fdc96feb7ef94c
Gitucc <54905593+Gitucc@users.noreply.github.com>
2021-12-21 15:11:52 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,8 +1,8 @@
 echo '修改机器名称'
-sed -i 's/OpenWrt/G-DOCK/g' package/base-files/files/bin/config_generate
+#sed -i 's/OpenWrt/G-DOCK/g' package/base-files/files/bin/config_generate
 
 echo '修改网关地址'
-sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 
 echo '修改时区'
 sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
@@ -21,11 +21,11 @@ sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/l
 #ln -s ../../../luci-theme-argon ./package/lean/
 
 echo '修改wifi名称'
-sed -i 's/OpenWrt/G-DOCK/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i 's/OpenWrt/G-DOCK/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 echo '修改banner'
-rm -rf package/base-files/files/etc/banner
-cp -f ../banner package/base-files/files/etc/
+#rm -rf package/base-files/files/etc/banner
+#cp -f ../banner package/base-files/files/etc/
 
 echo '下载ServerChan'
 #git clone https://github.com/tty228/luci-app-serverchan ../diy/luci-app-serverchan
